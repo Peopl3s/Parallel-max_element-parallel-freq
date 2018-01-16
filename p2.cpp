2576ea3bedd83c9cb1cc6421a554d43a91e4053b
Peopl3s <nikposd@mail.ru>
2018-01-16 20:20:09 +0300

    Update p2.cpp

--- p2.cpp
+++ p2.cpp
@@ -64,6 +64,58 @@ Iterator parallel_max(Iterator first,Iterator last)
     
 }
 
+
+template <typename Iterator>
+Iterator parallel_min_element(Iterator first, Iterator last, atomic<bool> &done) 
+{
+    try {
+        unsigned long const length=distance(first,last);
+        unsigned long const min_per_thread=10;
+ 
+        if(length<(2*min_per_thread)) 
+	{
+            if (first == last) return last;
+ 
+            Iterator smallest = first;
+            ++first;
+            for (; first != last; ++first) 
+	    {
+                if (*first < *smallest) 
+		{
+                    smallest = first;
+                }
+            }
+            return smallest;
+		
+        } else {
+            Iterator const mid_point=first+(length/2);
+            
+            future<Iterator> async_result=async(&parallel_min_element<Iterator>,mid_point,last,ref(done));
+            Iterator const direct_result=parallel_min_element(first,mid_point,done);
+            
+            Iterator direct=direct_result;
+            Iterator asyncR=async_result.get();
+ 
+            if(*direct<*asyncR) 
+	    {
+                return direct;
+            }
+            return asyncR;
+        }
+    } catch(...) 
+    {
+        done=true;
+        throw;
+    }
+}
+ 
+template <typename Iterator>
+Iterator parallel_min(Iterator first,Iterator last)
+{
+    atomic<bool>done(false);
+    return parallel_min_element(first,last,done);
+}
+
 int main() 
 {
     vector<int>v {1,11,2,0,3,4,9,5,7,3,14,78,45,567,121,578,2345,789,123,567,234,679,288,345};
